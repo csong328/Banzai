bdc3d0d2eac2af9fd36fbb7691ca29acaa64d5f2
Chen Song <chensong328@gmail.com>
2016-07-21 13:01:28 -0400

    fix

--- src/main/java/quickfix/examples/banzai/application/ApplicationConfig.java
+++ src/main/java/quickfix/examples/banzai/application/ApplicationConfig.java
@@ -1,21 +1,8 @@
 package quickfix.examples.banzai.application;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("quickfix.examples.banzai")
-public class ApplicationConfig {
-  @Bean
-  public Executor executor() {
-    return Executors.newCachedThreadPool(r -> {
-      Thread t = new Thread(r);
-      t.setDaemon(true);
-      return t;
-    });
-  }
-}
+public class ApplicationConfig {}
